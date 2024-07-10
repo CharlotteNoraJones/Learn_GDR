da6b848598da8ba19bb6db1bbf9834ac80d3932f
Charlotte Jones <Charlotte.Jones@armedicalboard.org>
2024-07-10 14:41:49 -0500

    Added code to open an image in the window.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,29 @@
 use std::time::Duration;
 
 use sdl2::{
-    event::Event, keyboard::Keycode, pixels::Color, render::WindowCanvas, sys::DefaultExposures,
+    event::Event,
+    image::{self, InitFlag, LoadTexture},
+    keyboard::Keycode,
+    pixels::Color,
+    render::{Texture, WindowCanvas},
 };
 
-fn render(canvas: &mut WindowCanvas, color: Color) {
+fn render(canvas: &mut WindowCanvas, color: Color, texture: &Texture) -> Result<(), String> {
     canvas.set_draw_color(color);
     canvas.clear();
+
+    canvas.copy(texture, None, None)?;
+
     canvas.present();
+
+    Ok(())
 }
 
 fn main() -> Result<(), String> {
     let sdl_context = sdl2::init()?;
     let video_subsystem = sdl_context.video()?;
+
+    let _image_context = image::init(InitFlag::PNG | InitFlag::JPG)?;
 
     let window = video_subsystem
         .window("game tutorial", 800, 600)
@@ -25,8 +36,11 @@ fn main() -> Result<(), String> {
         .build()
         .expect("Could not make a canvas.");
 
+    let texture_creator = canvas.texture_creator();
+    let texture = texture_creator.load_texture("assets/bardo.png")?;
+
     let mut event_pump = sdl_context.event_pump()?;
-    let mut i = 0;
+    let mut i: i32 = 0;
     'running: loop {
         // Handle events
         for event in event_pump.poll_iter() {
@@ -46,7 +60,11 @@ fn main() -> Result<(), String> {
         i = (i + 1) & 255;
 
         // Render
-        render(&mut canvas, Color::RGB(i, 64, 255 - i));
+        render(
+            &mut canvas,
+            Color::RGB(i as u8, 64, 255 - i as u8),
+            &texture,
+        )?;
 
         // Time Management
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
